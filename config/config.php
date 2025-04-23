648adf65a421021d34f98bea8ab90008e7285078
Dat1112 <chuthanhdat06@gmail.com>
2025-04-23 23:33:14 +0700

    them mat khau

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "admin";
 $database = "nhom8web"; // Đổi tên biến từ $sever thành $database
 
 // Create connection
